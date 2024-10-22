f7121d3abdac95c46fbbf86c675e027933233e00
Sebastian Choren <sebastian.choren@gmail.com>
2024-10-22 14:26:00 -0300

    fix script

--- tracetest-start.sh
+++ tracetest-start.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-# Archivo docker-compose.yaml
+set -e
+
+if ! command -v yq &> /dev/null
+then
+  printf "\e[41myq could not be found, please install it first\e[0m\n"
+  echo
+  echo "  see https://github.com/mikefarah/yq?tab=readme-ov-file#install"
+  exit 1
+fi
+
+if ! command -v tracetest &> /dev/null
+then
+  printf "\e[41mtracetest could not be found, please install it\e[0m\n"
+  echo
+  echo "    curl -L tracetest.io/install | sh"
+  exit 1
+fi
+
+if [ ! -f ~/.tracetest/config.yml ]; then
+  printf "\e[43mTracetest configuration file not found. Configuring tracetest CLI\e[0m\n"
+  tracetest configure
+fi
+
+API_KEY=$(tracetest list environmenttoken -o yaml | yq 'select(.spec.role == "admins" and .spec.isRevoked == false and document_index == 0) | .spec.id')
+ENDPOINT=$(cat ~/.tracetest/config.yml | yq '"\(.scheme)://\(.endpoint)/"')
+
 COMPOSE_FILE="docker-compose.yaml"
 
-# Declarar un array para almacenar los puertos
 declare -a ports
 
-# Leer y modificar el contenido en memoria
 updated_yaml=$(yq eval '.services |= with_entries(.value += {"pid": "service:autoinstrument"})' "$COMPOSE_FILE")
 
-# Extraer los puertos y almacenarlos en un array
 ports=($(echo "$updated_yaml" | yq eval '.services.*.ports[] | select(.)' | awk -F":" '{print $2}'))
 
-# Agregar un servicio hardcodeado con heredoc
 new_service=$(cat <<EOF
 image: grafana/beyla:latest
 privileged: true
 environment:
-  OTEL_EXPORTER_OTLP_ENDPOINT: "http://host.docker.internal:4317"
+  OTEL_EXPORTER_OTLP_ENDPOINT: "$ENDPOINT"
+  OTEL_EXPORTER_OTLP_HEADERS: "X-Tracetest-Token=$API_KEY"
   BEYLA_OPEN_PORT: "$(IFS=,; echo "${ports[*]}")"
   BEYLA_TRACE_PRINTER: "text"
   BEYLA_BPF_TRACK_REQUEST_HEADERS: "true"
-extra_hosts:
-  - "host.docker.internal:host-gateway"
 EOF
 )
 new_service_json=$(echo "$new_service" | yq eval -o=json -I0)
-echo "$new_service_json"
 
-# Añadir el servicio hardcodeado al YAML actualizado usando yq para mergear
 updated_yaml=$(echo "$updated_yaml" | yq eval '.services.autoinstrument = '"$new_service_json")
+
+tracetest dashboard
 
 echo "$updated_yaml" | docker compose -f - up
